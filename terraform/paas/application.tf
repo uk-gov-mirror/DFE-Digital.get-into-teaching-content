c21ef233768404067a15bd6022e2d804b6e9ee92
Steven Fawcett <steve.fawcett@capgemini.com>
2020-06-19 14:32:26 +0100

    change time out

--- terraform/paas/application.tf
+++ terraform/paas/application.tf
@@ -5,7 +5,7 @@ resource "cloudfoundry_app" "app_application" {
     stopped      = var.application_stopped
     strategy     = var.strategy
     memory       = 1024
-    healthcheck_timeout = 180
+    health_check_timeout = 179
     dynamic "service_binding" {
       for_each = data.cloudfoundry_user_provided_service.logging
       content {
